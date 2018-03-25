f4a71a7c94d5da834b2f9a2954361ecb56f0aabe
Chase Gibbons <chase@rentdynamics.com>
2018-03-25 01:35:37 -0600

    changed ng-packagr to point to a different package.json again, this time without deleting lib files (yet)

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
@@ -22,6 +23,17 @@ module.exports = function (config) {
     },
     angularCli: {
       environment: 'dev'
+    },
+    // the default configuration
+    junitReporter: {
+      outputDir: 'junit', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'junit.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: '@rd/forms', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: false, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {}, // key value pair of properties to add to the <properties> section of the report
+      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,

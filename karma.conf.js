9aa1ddebb26b9d1166f78311b33c5c13a417343f
Chase Gibbons <chase@rentdynamics.com>
2018-03-26 13:53:04 -0600

    test(karma.conf.js): changed suite name to @turbonemesis/angular-cli-lib

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     junitReporter: {
       outputDir: 'junit', // results will be saved as $outputDir/$browserName.xml
       outputFile: 'junit.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
-      suite: '@rd/forms', // suite will become the package name attribute in xml testsuite element
+      suite: '@turbonemesis/angular-cli-lib', // suite will become the package name attribute in xml testsuite element
       useBrowserName: false, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
       classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
